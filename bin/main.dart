bf0a64a74b192dc689ae2c98dcdf1d718247c00f
Matthew Gleich <matthewgleich@gmail.com>
2020-04-19 17:28:26 -0400

    🚀 Implement formatter with dartfmt or flutter format

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,16 @@
+// Dart imports:
 import 'dart:io';
 
+// Package imports:
+import 'package:process_run/process_run.dart';
+import 'package:process_run/which.dart';
+
+// Project imports:
 import 'package:import_sorter/files.dart' as files;
 import 'package:import_sorter/pubspec.dart' as pubspec;
 import 'package:import_sorter/sort.dart' as sort;
 
-void main() {
+void main() async {
   /*
   Getting the package name and dependencies/dev-dependencies
   Package name is one factor used to identify project imports
@@ -23,12 +29,19 @@ void main() {
   // Getting all the dart files for the project
   final dartFiles = files.dartFiles();
 
+  final useDartFmt = whichSync('dartfmt') is String;
+
   for (final filePath in dartFiles.keys) {
     File(filePath).writeAsStringSync(sort.sortImports(
       dartFiles[filePath],
       packageName,
       dependencies,
     ));
+    if (useDartFmt) {
+      await run('dartfmt', ['-w', filePath]);
+    } else {
+      await run('flutter', ['format', filePath]);
+    }
     print('✅ Formatted ${filePath.replaceAll(Directory.current.path, '')}');
   }
 }
