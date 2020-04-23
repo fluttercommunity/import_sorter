0d59e13f6f8035f26fb6fa0a1ab619445dfbb9da
Matthew Gleich <matthewgleich@gmail.com>
2020-04-23 08:39:03 -0400

    🚀 Include transitive dependencies

--- bin/main.dart
+++ bin/main.dart
@@ -3,29 +3,38 @@ import 'dart:io';
 
 // 📦 Package imports:
 import 'package:yaml/yaml.dart';
+import 'package:process_run/process_run.dart';
 
 // 🌎 Project imports:
 import 'package:import_sorter/files.dart' as files;
 import 'package:import_sorter/sort.dart' as sort;
 
-void main(List<String> args) {
+void main(List<String> args) async {
   /*
   Getting the package name and dependencies/dev-dependencies
   Package name is one factor used to identify project imports
   Dependencies/dev-dependency names are used to identify package imports
   */
-  final file = File('${Directory.current.path}/pubspec.yaml');
-  final pubspecContents = loadYaml(file.readAsStringSync());
+  final pubspecYamlFile = File('${Directory.current.path}/pubspec.yaml');
+  final pubspecYaml = loadYaml(pubspecYamlFile.readAsStringSync());
 
   // Getting all dependencies and package name
-  final packageName = pubspecContents['name'];
+  final packageName = pubspecYaml['name'];
   final dependencies = [];
-  if (pubspecContents.containsKey('dependencies')) {
-    dependencies.addAll(pubspecContents['dependencies'].keys);
+  if (pubspecYaml.containsKey('dependencies')) {
+    if (pubspecYaml['dependencies'].keys.contains('flutter')) {
+      print('🏃‍♂️ Running: flutter pub get');
+      await run('flutter', ['pub', 'get']);
+      print('\t✅ Ran flutter pub get\n');
+    } else {
+      print('🏃‍♂️ Running: pub get');
+      await run('pub', ['get']);
+      print('\t✅ Ran flutter pub get\n');
+    }
   }
-  if (pubspecContents.containsKey('dev_dependencies')) {
-    dependencies.addAll(pubspecContents['dev_dependencies'].keys);
-  }
+  final pubspecLockFile = File('${Directory.current.path}/pubspec.lock');
+  final pubspecLock = loadYaml(pubspecLockFile.readAsStringSync());
+  dependencies.addAll(pubspecLock['packages'].keys);
 
   // Getting all the dart files for the project
   final dartFiles = files.dartFiles();
